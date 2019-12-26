f39bd1452223bb49a436668c26e5659fe8d7cf62
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-26 15:57:06 -0800

    initial commit, basic config

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const WebpackPwaManifest = require('webpack-pwa-manifest')
+
+module.exports = (env, { mode }) => {
+	const config = {
+		module: {
+			rules: [
+				{
+					test: /\.js$/, 
+					exclude: /node_modules/, 
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env', '@babel/preset-react'],
+							plugins: ['@babel/plugin-transform-runtime']
+						}
+					}
+				},
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'raw-loader']
+				}
+			]
+		},
+		plugins: [ new HtmlWebpackPlugin({ template: './src/index.html' }) ]
+	}
+	if (mode === 'development') {
+		config.devtool = 'cheap-module-eval-source-map'
+		config.devServer = { historyApiFallback: true }
+	}
+	if (mode === 'production') {
+		config.devtool = 'cheap-module-source-map'
+		config.plugins.push(
+			new CompressionPlugin(),
+			new CopyWebpackPlugin([
+				{ from: './_redirects', to: '_redirects', toType: 'file' },
+				{ from: './src/sw.js', to: 'sw.js', toType: 'file' }
+			]),
+			new WebpackPwaManifest({
+				name: 'Ziro Afiliados',
+				short_name: 'Afiliados',
+				start_url: '/',
+				background_color: '#FFF',
+				theme_color: '#FFF',
+				display: 'standalone',
+				icons: [{ src: './logo.png', sizes: [96, 128, 192, 256, 384, 512] }]
+			})
+		)
+	}
+	return config
+}
