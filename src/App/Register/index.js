f8c44891758fb6de357102eb0334277e4e9d60e7
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-28 09:54:52 -0800

    add welcome message

--- src/App/Register/index.js
+++ src/App/Register/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import HeaderHome from '@bit/vitorbarbosa19.ziro.header-home'
 import { containerWithPadding } from '@ziro/theme'
+import { welcome, marker } from './styles'
 
-const Register = () => {
-	return (
-		<div style={containerWithPadding}>
-			<HeaderHome linkPath='/login' linkText='Tem cadastro? LOGIN' />
-		</div>
-	)
-}
+const Register = () =>
+	<div style={containerWithPadding}>
+		<HeaderHome linkPath='/login' linkText='Tem cadastro? LOGIN' />
+		<h1 style={welcome}>
+			Crie sua conta<br/>
+			de <span style={marker}>afiliado</span>,
+		</h1>
+	</div>
 
 export default Register
