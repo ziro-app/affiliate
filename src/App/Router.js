dce69c0038d0656424b47c977e5b41be788eb47f
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-28 18:09:41 -0800

    update router

--- src/App/Router.js
+++ src/App/Router.js
@@ -1,22 +1,20 @@
 import React from 'react'
-import { useLocation } from 'wouter'
 import routeMatcher from '@ziro/router'
 import Login from './Login/index'
 import Register from './Register/index'
 import NotFound from '@bit/vitorbarbosa19.ziro.not-found'
 
 const Router = () => {
-	const [location] = useLocation()
 	const isLogged = false
 	const publicRoutes = {
-		'/home': <div>Home</div>,
 		'/login': <Login />,
 		'/cadastrar': <Register />
 	}
 	const privateRoutes = {
 		'/dashboard': <div>Dashboard</div>
 	}
-	return routeMatcher(location, isLogged, publicRoutes, privateRoutes, <NotFound fallback='/home' />)
+	const homeRoute = '/dashboard'
+	return routeMatcher(isLogged, publicRoutes, privateRoutes, homeRoute, <NotFound fallback='/' />)
 }
 
 export default Router
