7a113daa76cd17e6d6360a30c85696a916dd2f2e
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-30 13:39:58 -0800

    add signin

--- src/App/Login/sendToBackend.js
+++ src/App/Login/sendToBackend.js
@@ -1,4 +1,24 @@
-const sendToBackend = state => () =>
-	new Promise((res, rej) => setTimeout(() => res(console.log(state)), 2000))
+import { auth } from '../../Firebase/index'
+
+const sendToBackend = state => () => {
+	const { email, pass } = state
+	return new Promise(async (resolve, reject) => {
+		try {
+			await auth.signInWithEmailAndPassword(email, pass)
+		} catch (error) {
+			console.log(error)
+			if (error.code) {
+				switch (error.code) {
+					case 'auth/network-request-failed': reject({ msg: 'Sem conexão com a rede', customError: true })
+					case 'auth/invalid-email': reject({ msg: 'Email inválido', customError: true })
+					case 'auth/user-disabled': reject({ msg: 'Usuário bloqueado', customError: true })
+					case 'auth/user-not-found': reject({ msg: 'Usuário não cadastrado', customError: true })
+					case 'auth/wrong-password': reject({ msg: 'Senha incorreta', customError: true })
+					case 'auth/too-many-requests': reject({ msg: 'Muitas tentativas. Tente mais tarde', customError: true })
+				}
+			}
+		}
+	})
+}
 
 export default sendToBackend
