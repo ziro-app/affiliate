d65fd51fe02324135625dd67cb23db94a081d07e
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-30 11:56:33 -0800

    add auth state observer

--- src/App/Router.js
+++ src/App/Router.js
@@ -1,12 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import routeMatcher from '@ziro/router'
 import Login from './Login/index'
 import Register from './Register/index'
 import EmailConfirmation from './EmailConfirmation/index'
 import NotFound from '@bit/vitorbarbosa19.ziro.not-found'
 
-const Router = () => {
-	const isLogged = false
+const Router = ({ isLogged }) => {
 	const publicRoutes = {
 		'/login': <Login />,
 		'/cadastrar': <Register />,
@@ -17,6 +17,10 @@ const Router = () => {
 	}
 	const homeRoute = '/indicar'
 	return routeMatcher(isLogged, publicRoutes, privateRoutes, homeRoute, <NotFound fallback='/' />)
+}
+
+Router.propTypes = {
+	isLogged: PropTypes.bool.isRequired
 }
 
 export default Router
--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { auth } from '../Firebase/index'
 import ErrorBoundary from '@bit/vitorbarbosa19.ziro.error-boundary'
 import Router from './Router'
 
 export const App = () => {
+	const [user, setUser] = useState('')
+	useEffect(() => {
+		return auth.onAuthStateChanged(async user => {
+			if (user && user.emailVerified) {
+				console.log(user)
+				setUser(user)
+			}
+		})
+	}, [])
 	return (
 		<ErrorBoundary>
-			<Router />
+			<Router isLogged={!!user} />
 		</ErrorBoundary>
 	)
 }
