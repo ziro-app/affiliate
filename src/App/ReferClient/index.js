6d95dc7a1ca53ba35c4520894dff20c858aa06f8
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-30 14:10:43 -0800

    add page to refer new clients

---
+++ src/App/ReferClient/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Menu } from '../Menu/index'
+
+const ReferClient = () =>
+	<Menu title='Indicar Lojista'>
+		<div>Indicar lojista</div>
+	</Menu>
+
+export default ReferClient
--- src/App/Router.js
+++ src/App/Router.js
@@ -4,6 +4,7 @@ import routeMatcher from '@ziro/router'
 import Login from './Login/index'
 import Register from './Register/index'
 import EmailConfirmation from './EmailConfirmation/index'
+import ReferClient from './ReferClient/index'
 import NotFound from '@bit/vitorbarbosa19.ziro.not-found'
 
 const Router = ({ isLogged }) => {
@@ -13,7 +14,7 @@ const Router = ({ isLogged }) => {
 		'/confirmar-email': <EmailConfirmation />
 	}
 	const privateRoutes = {
-		'/indicar': <div>Indicação de clientes</div>
+		'/indicar': <ReferClient />
 	}
 	const homeRoute = '/indicar'
 	return routeMatcher(isLogged, publicRoutes, privateRoutes, homeRoute, <NotFound fallback='/' />)
