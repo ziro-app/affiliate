eab49ad029c934730f221cc6c0217ac6dc8f6dcf
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-01-04 11:03:37 -0800

    wrangle data fetched

--- src/App/Register/fetch.js
+++ src/App/Register/fetch.js
@@ -8,7 +8,7 @@ const fetch = () => {
 			apiResource: 'values',
 			apiMethod: 'get',
 			spreadsheetId: process.env.SHEET_ID_REGISTER_GET,
-			range: 'Dados!W:W'
+			range: 'Dados!W:AJ'
 		},
 		headers: {
 			'Authorization': process.env.SHEET_TOKEN,
@@ -17,10 +17,21 @@ const fetch = () => {
 	}
 	const runFetch = async () => {
 		try {
-			const { data } = await axios(config)
-			console.log(data)
+			const { data: { values } } = await axios(config)
+			const [, ...dataWithoutHeaderRow] = values
+			const brandsAndAddresses = dataWithoutHeaderRow.map(value => {
+				const [brand, , ...addresses] = value
+				let fullAddresses = []
+				for (let i = 0; i < addresses.length; i++) {
+					if (i % 2 === 0)
+						fullAddresses.push(`${brand} - ${addresses[i]}, ${addresses[i + 1]}`)
+				}
+				return fullAddresses
+			}).flat()
+			console.log(brandsAndAddresses)
 		} catch (error) {
-			console.log(error.response)
+			if (error.response) console.log(error.response)
+			else console.log(error)
 		}
 	}
 	runFetch()
