bd9316ee4d009a94a426f6a728aee037dd177d64
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-01-06 14:13:21 -0800

    add error loading logic

--- src/App/index.js
+++ src/App/index.js
@@ -5,6 +5,7 @@ import ErrorBoundary from '@bit/vitorbarbosa19.ziro.error-boundary'
 import Router from './Router'
 
 export const App = () => {
+	const [errorLoading, setErrorLoading] = useState(false)
 	const [uid, setUid] = useState('')
 	const [name, setName] = useState('')
 	const [cpf, setCpf] = useState('')
@@ -17,19 +18,26 @@ export const App = () => {
 	useEffect(() => {
 		const getUserData = async () => {
 			if (uid) {
-				const docRef = await db.collection('affiliates').where('uid','==',uid).get()	
-				if (!docRef.empty) {
-					docRef.forEach(doc => {
-						const data = doc.data()
-						setName(`${data.fname} ${data.lname}`)
-						setCpf(data.cpf)
-					})
+				try {
+					const docRef = await db.collection('affiliates').where('uid','==',uid).get()	
+					if (!docRef.empty) {
+						docRef.forEach(doc => {
+							const data = doc.data()
+							setName(`${data.fname} ${data.lname}`)
+							setCpf(data.cpf)
+						})
+					} else setErrorLoading(true)
+				} catch (error) {
+					if (error.response) console.log(error.response)
+					else console.log(error)
+					setErrorLoading(true)	
 				}
 			}
 		}
 		getUserData()
 	}, [uid])
 	const userData = { uid, name, cpf }
+	if (errorLoading) return <div>Erro na página. Tente novamente ou solicite suporte</div>
 	return (
 		<ErrorBoundary>
 			<userContext.Provider value={userData}>
