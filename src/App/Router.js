2a7167db579b0bbfeed79c04c433b9451b75a070
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-01-24 21:39:51 -0800

    add user management components

--- src/App/Router.js
+++ src/App/Router.js
@@ -11,6 +11,10 @@ import { Menu } from './Menu/index'
 import ReferClient from './ReferClient/index'
 import MyAccount from '@bit/vitorbarbosa19.ziro.my-account'
 import About from '@bit/vitorbarbosa19.ziro.about'
+import UpdateEmail from '@bit/vitorbarbosa19.ziro.update-email'
+import UpdatePass from '@bit/vitorbarbosa19.ziro.update-pass'
+import Reauthenticate from '@bit/vitorbarbosa19.ziro.reauthenticate'
+import DeleteAccount from '@bit/vitorbarbosa19.ziro.delete-account'
 import NotFound from '@bit/vitorbarbosa19.ziro.not-found'
 
 const Router = ({ isLogged }) => {
@@ -25,7 +29,11 @@ const Router = ({ isLogged }) => {
 	const privateRoutes = { // Menu can't be put inside the components because then it'll unmount on transition
 		'/indicar': <Menu title='Indicar Lojista'><ReferClient /></Menu>,
 		'/conta': <Menu title='Minha Conta'><MyAccount /></Menu>,
-		'/ziro': <Menu title='Sobre a Ziro'><About /></Menu>
+		'/ziro': <Menu title='Sobre a Ziro'><About /></Menu>,
+		'/trocar-email': <UpdateEmail />,
+		'/trocar-senha': <UpdatePass />,
+		'/reautenticar': <Reauthenticate />,
+		'/deletar-conta': <DeleteAccount />
 	}
 	const homeRoute = '/indicar'
 	return routeMatcher(isLogged, publicRoutes, privateRoutes, homeRoute, <NotFound fallback='/' />)
