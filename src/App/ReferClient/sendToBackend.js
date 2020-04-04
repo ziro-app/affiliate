b69c6cb2f70f436ae7843dd6eaaf6db4c2ffd539
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-04-04 16:11:40 -0700

    updates

--- src/App/ReferClient/sendToBackend.js
+++ src/App/ReferClient/sendToBackend.js
@@ -26,23 +26,6 @@ const sendToBackend = state => () => {
 		},
 		valueInputOption: 'user_entered'
 	}
-	const bodyLegacy = {
-		apiResource: 'values',
-		apiMethod: 'append',
-		spreadsheetId: process.env.SHEET_ID_REFER_LEGACY,
-		range: 'Clientes!A1',
-		resource: {
-			values: [
-				[`${fnameTrim.toUpperCase()} ${lnameTrim.toUpperCase()}`,
-					rg, cpf, cnpj, ie, razao, fantasia,
-				complemento ? `${rua}, ${numero}, ${complemento}` : `${rua}, ${numero}`,
-					bairro, cep, cidade, estado, fone.replace(' ', ''), email, ,
-				today.toLocaleString('en-GB').replace(',', ''), today.getMonth() + 1, today.getFullYear(),
-					'ativo', , , affiliateName.toUpperCase(), affiliateCpf]
-			]
-		},
-		valueInputOption: 'user_entered'
-	}
 	const config = {
 		headers: {
 			'Content-type': 'application/json',
@@ -54,42 +37,35 @@ const sendToBackend = state => () => {
 			if (cnpjValid) {
 				await post(url, body, config)
 				try {
-					await post(url, bodyLegacy, config)
-					try {
-						await db.collection('storeowners').add({
-							cadastro: today,
-							nomeAfiliado: 'NENHUM',
-							cpfAfiliado: '',
-							fname: fnameTrim,
-							lname: lnameTrim,
-							rg,
-							cpf,
-							nascimento: birth,
-							instagram: instaTrim,
-							cnpj,
-							ie,
-							razao,
-							fantasia,
-							endereco: complemento ? `${rua}, ${numero}, ${complemento}` : `${rua}, ${numero}`,
-							bairro,
-							cep,
-							cidade,
-							estado,
-							fone,
-							whatsapp: whats,
-							email: email.toLowerCase(),
-							assessor: 'NENHUM',
-							vendedor: 'NENHUM'
-						})
-					} catch (error) {
-						console.log(error)
-						if (error.response) console.log(error.response)
-						throw 'Erro ao salvar na Firestore'
-					}
+					await db.collection('storeowners').add({
+						cadastro: today,
+						nomeAfiliado: 'NENHUM',
+						cpfAfiliado: '',
+						fname: fnameTrim,
+						lname: lnameTrim,
+						rg,
+						cpf,
+						nascimento: birth,
+						instagram: instaTrim,
+						cnpj,
+						ie,
+						razao,
+						fantasia,
+						endereco: complemento ? `${rua}, ${numero}, ${complemento}` : `${rua}, ${numero}`,
+						bairro,
+						cep,
+						cidade,
+						estado,
+						fone,
+						whatsapp: whats,
+						email: email.toLowerCase(),
+						assessor: 'NENHUM',
+						vendedor: 'NENHUM'
+					})
 				} catch (error) {
 					console.log(error)
 					if (error.response) console.log(error.response)
-					throw error
+					throw 'Erro ao salvar na Firestore'
 				}
 				// clear all fields after submission
 				setSearchedName('')
